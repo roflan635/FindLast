ced41648b0a263c3a5eadeff7ce8c75ae19d25ba
roflan635 <nikolaev.nikita2281337@gmail.com>
2025-02-04 18:07:09 +0400

    изи о

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -4,10 +4,21 @@
 #include <iostream>
 #include "Task.h"
 
-
+int FindLast(int* arr, int elements, int value)
+{
+	if (elements == 0 || arr == nullptr) return -1;
+	else
+	{
+		for (int i = elements - 1; i <= elements; i--)
+			if (arr[i] == value) return i;
+		return -1;
+	}
+}
 int main()
-{;
-	// Здесь должен быть ваш код
-
+{
+	int m[] = { 1, 2,2,2,2,2,2,2, 3,4,5,6,7,8,9, 10 };
+	int size = sizeof(m) / sizeof(int);
+	int value = 2;
+	std::cout << FindLast(m, size, value);
 }
 
